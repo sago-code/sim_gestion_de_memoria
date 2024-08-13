40d75d5fd6327f2f3a2190350a8ca99ad86806ca
sago-code <139510724+sago-code@users.noreply.github.com>
2024-08-13 18:16:10 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import { Fragment } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { SimGestionMemoria } from './components/modules/SImGestionMemoria';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Fragment>
+          <BrowserRouter>
+            <Routes>
+              <Route exact path='' element={<SimGestionMemoria/>}/>
+            </Routes>
+          </BrowserRouter>
+        </Fragment>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/modules/SImGestionMemoria.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from "react";
+
+export function SimGestionMemoria() {
+    return(
+        <Fragment>
+            <h1>QUE ONDAAAAAAAAAAA</h1>
+        </Fragment>
+    );
+}
