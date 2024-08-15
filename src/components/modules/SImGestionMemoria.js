cf0afc7e91a614d94f4d38fce4948c2846d0e3c6
sago-code <139510724+sago-code@users.noreply.github.com>
2024-08-15 18:45:36 -0500

    Update SImGestionMemoria.js

--- src/components/modules/SImGestionMemoria.js
+++ src/components/modules/SImGestionMemoria.js
@@ -3,7 +3,21 @@ import React, { Fragment } from "react";
 export function SimGestionMemoria() {
     return(
         <Fragment>
-            <h1>QUE ONDAAAAAAAAAAA</h1>
+            <div>
+                <div>
+                    <h4>Simulador de gestion de memoria</h4>
+                <div>
+                <div>
+                    <form>
+                        <div>
+                            <text>ingrese la tarea</text>
+                            <input
+                                placeholder="ejemplo word"
+                            />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </Fragment>
     );
 }
