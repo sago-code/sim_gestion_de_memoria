779cbe92de6a549ddd40ce1fd3e29eed4bbe96d4
sago-code <139510724+sago-code@users.noreply.github.com>
2024-08-17 09:04:51 -0500

    Update SImGestionMemoria.js

--- src/components/modules/SImGestionMemoria.js
+++ src/components/modules/SImGestionMemoria.js
@@ -2,19 +2,32 @@ import React, { Fragment } from "react";
 import '../styles/SimGestionMemoria.css';
 
 const datosMemoria = [
-    {tamaño: '12MB' },
-    {tamaño: '24MB' },
-    {tamaño: '40MB'},
-    {tamaño: '60MB'}
+    {tamaño: '50KB' },
+    {tamaño: '80KB' },
+    {tamaño: '100KB'},
+    {tamaño: '150KB'},
+    {tamaño: '200KB'},
+    {tamaño: '230KB'}
 ];
 
 export function SimGestionMemoria() {
     return(
         <Fragment>
+            <div>
+                <form>
+                    <div>
+                        <text>Proceso</text>
+                        <input
+                            placeholder: "tarea, programa o proceso"
+                        />
+                        <button>Enviar</button> 
+                    </div>
+                </form>
+            <div>
             <table className="tabla-memoria">
                 <thead>
                     <tr>
-                        <th>memoria ram</th>
+                        <th>Sistema Operativo</th>
                     </tr>
                 </thead>
                 <tbody>
